c4f5aea14d545c523624284b2a438cde3a8b53af
O.Bilenko <oleksandr.bilenko@shopgate.com>
2018-07-03 13:29:13 +0200

    Change subscriptions for checkout. Increase version to beta 7

--- frontend/Checkout/subscribers.js
+++ frontend/Checkout/subscribers.js
@@ -1,17 +1,43 @@
 import { main$ } from '@shopgate/pwa-common/streams/main';
 import { getUserData } from '@shopgate/pwa-common/selectors/user';
+import { userDidLogin$, userDataReceived$ } from '@shopgate/pwa-common/streams/user';
+import getUser from '@shopgate/pwa-common/actions/user/getUser';
 
 export default (subscribe) => {
   const checkoutEnter$ = main$.filter(({ action }) => action.type === 'CHECKOUT_ENTER');
 
   /**
+   * After entering checkout, user data can be fetched (after login)
+   * Zip both streams to have 1 subscriber to set checkout data
+   */
+  const userDataReceivedCheckout$ = checkoutEnter$
+    .zip(userDataReceived$)
+    .map(([checkoutEnterAction]) => checkoutEnterAction);
+
+  /**
+   * Merge streams of entering into checkout and user data
+   * to update checkout state
+   */
+  const checkoutEnterUser$ = checkoutEnter$.merge(userDataReceivedCheckout$);
+
+  /**
+   * Request user data with addresses, if addresses not fetched yet
+   */
+  subscribe(userDidLogin$, ({ dispatch, getState }) => {
+    const { addresses } = getUserData(getState());
+    if (!addresses) {
+      dispatch(getUser());
+    }
+  });
+
+  /**
    * Add addresses selection to checkout
    */
-  subscribe(checkoutEnter$, ({ dispatch, getState, action }) => {
+  subscribe(checkoutEnterUser$, ({ dispatch, getState, action }) => {
     const { addresses } = getUserData(getState());
     const { checkout } = action;
 
-    if (!checkout.shippingAddress) {
+    if (!checkout.shippingAddress && addresses) {
       const shipAddress = addresses.find(address => address.tags && address.tags.includes('shipping'));
       if (shipAddress) {
         const { tags: ignore, ...restAddress } = shipAddress;
@@ -23,7 +49,7 @@ export default (subscribe) => {
       }
     }
 
-    if (!checkout.billingAddress) {
+    if (!checkout.billingAddress && addresses) {
       const bilAddress = addresses.find(address => address.tags && address.tags.includes('billing'));
       if (bilAddress) {
         const { tags: ignore, ...restAddress } = bilAddress;
