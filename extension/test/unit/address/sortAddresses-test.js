c8fac31c44d3478d7ac42a635ce62e63979a5741
Rene Eichhorn <rene.eichhorn@shopgate.com>
2018-08-27 14:50:57 +0200

    PWA-880 - added unit tests for sorting

---
+++ extension/test/unit/address/sortAddresses-test.js
@@ -0,0 +1,53 @@
+const assert = require('assert')
+const stepExecute = require('../../../address/sortAddresses')
+
+const out = {}
+const context = {
+  storage: {
+    user: {
+      get: (key) => {},
+      set: (key, val) => { out[key] = val }
+    }
+  }
+}
+
+describe('sortAddresses', () => {
+  it('should handle empty array', async () => {
+    context.storage.user.get = () => []
+    await stepExecute(context, null)
+
+    assert.deepEqual(out.addresses, [])
+  })
+
+  it('should move default to top', async () => {
+    context.storage.user.get = () => [
+      { id: 1, tags: [] },
+      { id: 2, tags: ['default'] },
+      { id: 3, tags: ['foo'] }
+    ]
+    await stepExecute(context, null)
+
+    assert.deepEqual(out.addresses, [
+      { id: 2, tags: ['default'] },
+      { id: 1, tags: [] },
+      { id: 3, tags: ['foo'] }
+    ])
+  })
+
+  it('should prioities multiple default addresses', async () => {
+    context.storage.user.get = () => [
+      { id: 1, tags: [] },
+      { id: 2, tags: ['default_payment'] },
+      { id: 3, tags: ['foo'] },
+      { id: 4, tags: ['default_shipping'] }
+    ]
+    await stepExecute(context, null)
+
+    assert.deepEqual(out.addresses, [
+      { id: 2, tags: ['default_payment'] },
+      { id: 4, tags: ['default_shipping'] },
+      { id: 1, tags: [] },
+      { id: 3, tags: ['foo'] }
+    ])
+  })
+})
