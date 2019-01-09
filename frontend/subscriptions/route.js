3f0dec72929deda74577c68f33b77ed4db887fcc
Waldemar Rutkowski <SG-Noxoreos@users.noreply.github.com>
2019-01-09 17:51:05 +0100

    Adapted a change that was recently performed forPWA 6.1.0

--- frontend/subscriptions/route.js
+++ frontend/subscriptions/route.js
@@ -1,5 +1,5 @@
 import { appDidStart$ } from '@shopgate/pwa-common/streams/app';
-import { authRouter } from '@shopgate/pwa-common/collections';
+import { authRoutes } from '@shopgate/pwa-common/collections';
 import { LOGIN_PATH } from '@shopgate/pwa-common/constants/RoutePaths';
 import * as path from '../constants/RoutePaths';
 
@@ -8,8 +8,8 @@ import * as path from '../constants/RoutePaths';
  */
 export default (subscribe) => {
   subscribe(appDidStart$, () => {
-    authRouter.set(path.USER_PROFILE_PATH, LOGIN_PATH);
-    authRouter.set(path.USER_ADDRESS_BOOK_PATH, LOGIN_PATH);
-    authRouter.set(path.USER_ADDRESS_PATH, LOGIN_PATH);
+    authRoutes.set(path.USER_PROFILE_PATH, LOGIN_PATH);
+    authRoutes.set(path.USER_ADDRESS_BOOK_PATH, LOGIN_PATH);
+    authRoutes.set(path.USER_ADDRESS_PATH, LOGIN_PATH);
   });
 };
